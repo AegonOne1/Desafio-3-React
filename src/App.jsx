ab7ce3f3367808f2243b2eafce53d8c825a4b065
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-02-21 17:29:19 -0300

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import TableList from './components/Listado'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+
+  return (
+    <>
+    <TableList />
+    </>
+  )
+}
+
+export default App
